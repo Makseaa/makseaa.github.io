0ef7836e929bbb0a1be14ecbb7ca97dd7a88df8a
Mac Cea <makseaa@gmail.com>
2023-09-13 13:03:17 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,9 @@
+number=0
+def initiateCalculator():
+    num=input("Enter your number: ")
+    calculatenumber(num)
 
+def calculatenumber(num)
+     option=input("select your option: ['sum','multiply','divide']: ")
+
+initiateCalculator(num)
